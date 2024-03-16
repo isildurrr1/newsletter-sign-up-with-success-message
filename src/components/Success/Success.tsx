cbc134a50ad993e0e66a17e15ca16c15f934ac90
Andrey Romashev <andreygaranin.0@yandex.ru>
2024-03-16 14:48:23 +0700

    add paragraph

--- src/components/Success/Success.tsx
+++ src/components/Success/Success.tsx
@@ -1,12 +1,19 @@
 import './Success.sass'
 import image from '../../images/icon-success.svg'
+import { useState } from 'react'
 
 const Success = () => {
+  const [email, sentEmail] = useState('ash@loremcompany.com')
   return (
     <div className="success">
       <div className="container">
         <img src={image} alt="img" className="image" />
         <h1 className="title">Thanks for subscribing!</h1>
+        <p className="paragraph">A confirmation email has been sent to
+          <span className="email"> {email}</span>.
+          Please open it and click the button inside to confirm your subscription.
+        </p>
+        <button className="button">Dismiss message</button>
       </div>
     </div>
   )
